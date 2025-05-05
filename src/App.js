07b6ae1f4ae0e58efdf656e4bc07976c40dc56c7
SarahNk1-Maker <sarahnaveed216@gmail.com>
2025-05-04 23:05:24 -0400

    fixed the app.js error

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
-import Contact from './pages/contact'; // Adjust the path if needed
-import Footer from './pages/footer'; // Adjust the path if needed
-import Home from './pages/LandingPage'; // Adjust the path if needed
-import AboutUs from './pages/about-us'; // Adjust the path if needed
-import Header from './pages/header'; // Adjust the path if needed
-import Equipment from './pages/equipment'; // Adjust the path if needed
+import Contact from './pages/contact';
+import Footer from './pages/footer';
+import Home from './pages/LandingPage';
+import AboutUs from './pages/about-us';
+import Header from './pages/header';
+import Equipment from './pages/equipment';
+
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/equipment" element={<Equipment />} />
       </Routes>
-      <Header/>
       <Footer />
-     
     </>
   );
 }
